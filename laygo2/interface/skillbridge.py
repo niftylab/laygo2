4ca7def97a543e739aa90d192c975781ebe4bd0a
Park-Hyung-Joo <copyrose97@gmail.com>
2022-01-11 15:55:16 +0900

    Workspace.open() add parameter. os.environ['USER']

--- laygo2/interface/skillbridge.py
+++ laygo2/interface/skillbridge.py
@@ -30,9 +30,9 @@ def export(db, filename, cellname, scale=1e-3, reset_library=False, tech_library
     skill_str=laygo2.interface.skill.export(db, filename, cellname, scale, reset_library, tech_library)
 
     # Export skill script to skillbridge
-
+    import os
     from skillbridge import Workspace
-    ws = Workspace.open()
+    ws = Workspace.open(os.environ['USER'])
     ws['load'](filename)
 
     return skill_str
