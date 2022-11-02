3a9e1b5c5bb3d666503b289a60f2fd8fa9f98282
Jaeduk Han <jdhan@hanyang.ac.kr>
2022-11-02 22:46:29 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,40 @@
 # -*- coding: utf-8 -*-
 
-'''
+"""
 Pypi update instruction:
 1) update the version number in setup.py.
 2) run 'python setup.py sdist bdist_wheel' to build whl.
 3) run 'python -m twine upload dist/*'.
 4) type username and password.
 
-'''
+"""
 
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='laygo2',
-    version='0.2.8',
-    author='Jaeduk Han',
-    description='LAYout with Gridded Object 2 - A Python Package for Scripted IC Layout Generation Flow',
+    name="laygo2",
+    version="0.2.9",
+    author="Jaeduk Han",
+    description="LAYout with Gridded Object 2 - A Python Package for Scripted IC Layout Generation Flow",
     long_description=readme,
-    url='https://github.com/niftylab/laygo2', # project address
+    url="https://github.com/niftylab/laygo2",  # project address
     license=license,
-    packages=find_packages(exclude=('test', 'docs')),
-    package_data={'': ['interface/skill_export.il', 
-                       'interface/magic_export.tcl', 
-                       'interface/gds_default.layermap', 
-                       'examples/laygo2_tech/laygo2_tech.yaml', 
-                       'examples/laygo2_tech/laygo2_tech.layermap', 
-                      ]},
+    packages=find_packages(exclude=("test", "docs")),
+    package_data={
+        "": [
+            "interface/skill_export.il",
+            "interface/magic_export.tcl",
+            "interface/gds_default.layermap",
+            "examples/laygo2_tech/laygo2_tech.yaml",
+            "examples/laygo2_tech/laygo2_tech.layermap",
+        ]
+    },
     include_package_data=True,
-    python_requires='>=3.0',
+    python_requires=">=3.0",
 )
