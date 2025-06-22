9e75417d099396dd3c37cbdc43fef00356b1dcd2
Jaeduk Han <jdhan@hanyang.ac.kr>
2025-06-22 13:40:53 +0900

    Update routing.py

--- laygo2/object/grid/routing.py
+++ laygo2/object/grid/routing.py
@@ -24,7 +24,7 @@
 
 #import numpy as np
 #from .core import CircularMapping, Grid, OneDimGrid
-#import laygo2.object
+import laygo2.object
 import numpy as np
 from .core import CircularMapping, Grid, OneDimGrid
 from ..physical import PhysicalObject, Rect, Path, Pin, Text, Instance, VirtualInstance, PhysicalObjectPointer, PhysicalObjectPointerAbsCoordinate
